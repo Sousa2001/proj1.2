862e6d21225fc9c1df779120bea4905bf043c701
linux <linux@debian10.4>
2020-12-17 19:58:29 +0100

    TODO ACTUALIZADO PROD V2

--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -10,9 +10,9 @@
     <header>
         <h1>TODO LIST</h1>
     <nav am-layout="horizontal">
-        <a href="/user">Home</a>
-        <a href="/user/dashboard">Dashboard</a>
-        <a href="/login">Logout</a>
+        <a href="<?= BASE;?>user">Home</a>
+        <a href="<?= BASE;?>user/dashboard">Dashboard</a>
+        <a href="<?= BASE;?>login">Logout</a>
     </nav>
     </header>
 </body>
